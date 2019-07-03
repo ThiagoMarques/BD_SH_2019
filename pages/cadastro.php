f936a15c075fdabf5d9de2d43b104a974fb1ecac
Thiago Marques <thiagossmarques@gmail.com>
2019-07-03 13:26:02 -0300

    Ajusta cadastro

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -24,58 +24,54 @@
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
-    <fieldset>
-        <legend>Preencha os campos</legend>
-        <form method="POST" action="../config/insert.php">
-            <form>
-                <div class="form-group">
-                    <label for="matricula">Matricula</label>
-                    <input type="text" class="form-control" name="matricula" id="matricula" placeholder="Digite a matricula">
-                </div>
-                <div class="form-group">
-                    <label for="nome">Nome Completo</label>
-                    <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite o nome">
-                </div>
-                <div class="form-group">
-                    <label for="senha">Senha</label>
-                    <input type="password" class="form-control" name="senha" id="senha" placeholder="Senha">
-                </div>
-                <div class="form-group">
-                    <label for="email">Endereço de email</label>
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Digite o email">
-                </div>
-                <div>
-                    <div class="form-group">
-                        <label for="data_nasc">Data de Nascimento</label>
-                        <input type="date" class="form-control" name="data_nasc" placeholder="" value="<?=$linha['Data_Nasc']?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="sex">Select de exemplo</label>
-                    <select class="form-control" name="sex" id="sex">
-                        <option selected disable>Selecione o sexo</option>
-                        <option value='M'>Masculino</option>
-                        <option value='F'>Feminino</option>
-                    </select>
-                </div>
-                <button type="submit" value="Cadastrar" class="btn btn-primary">Enviar</button>
-                <a href="/BD_SH_2019/principal.php"></a><button type="submit" value="Cadastrar" class="btn btn-primary">Voltar</button>
-            <!-- </form>
-            <input type="matricula" name="matricula" required placeholder="matricula" />
-            <input type="text" name="nome" required placeholder="nome" />
-            <input type="password" name="senha" required placeholder="senha" />
-            <input type="text" name="email" placeholder="Email" value="<?=$linha['Email']?>" required>
-            <input type="date" name="data_nasc" placeholder="" value="<?=$linha['Data_Nasc']?>" required>
-            <select name="sex">
-                <option selected disable>Selecione o sexo</option>
-                <option value='M'>Masculino</option>
-                <option value='F'>Feminino</option>
-            </select>
-            <input type="submit" value="Cadastrar" /> -->
-        </form>
-    </fieldset>
+    <div class="card">
+        <div class="card-body">
+            <div class="col-sm-8">
+                <fieldset>
+                    <legend>Preencha os campos</legend>
+                    <form method="POST" action="../config/insert.php">
+                        <div class="form-group">
+                            <label for="matricula">Matricula</label>
+                            <input type="text" class="form-control" name="matricula" id="matricula"
+                                placeholder="Digite a matricula">
+                        </div>
+                        <div class="form-group">
+                            <label for="nome">Nome Completo</label>
+                            <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite o nome">
+                        </div>
+                        <div class="form-group">
+                            <label for="senha">Senha</label>
+                            <input type="password" class="form-control" name="senha" id="senha" placeholder="Senha">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Endereço de email</label>
+                            <input type="email" class="form-control" name="email" id="email"
+                                placeholder="Digite o email">
+                        </div>
+                        <div>
+                            <div class="form-group">
+                                <label for="data_nasc">Data de Nascimento</label>
+                                <input type="date" class="form-control" name="data_nasc" placeholder=""
+                                    value="<?=$linha['Data_Nasc']?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="sex">Selecione o sexo</label>
+                            <select class="form-control" name="sex" id="sex">
+                                <option selected disable>Selecione o sexo</option>
+                                <option value='M'>Masculino</option>
+                                <option value='F'>Feminino</option>
+                            </select>
+                        </div>
+                        <button type="submit" value="Cadastrar" class="btn btn-primary btn-lg btn-block">Registrar Usuario</button>
+                    </form>
+                </fieldset>
+                <hr>
+                <a href="/BD_SH_2019/principal.php"><button class="btn btn-primary">Voltar</button></a>
+            </div>
+        </div>
+    </div>
     <hr>
-    <!-- <a href="/BD_SH_2019/principal.php"><button>Voltar</button></a> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
